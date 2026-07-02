fb7d88860c5ccf23d6697f4049269dff41d1a479
Abhinav <pasumarthyabhinav955@gmail.com>
2026-07-02 22:20:43 +0530

    Refactor import statement for authenticate middleware

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticate = require('../middleware/auth');
+const { authenticate } = require('../middleware/auth');
 const authorizeRoles = require('../middleware/roleCheck');
 const { getAll, getOne, create, update, remove, categoryValidation } = require('../controllers/categoryController');
 
@@ -11,3 +11,4 @@ router.put('/:id',  authenticate, authorizeRoles('admin'), update);
 router.delete('/:id', authenticate, authorizeRoles('admin'), remove);
 
 module.exports = router;
+
